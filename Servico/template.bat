65562bca83ab8df0e7342274dfe08820f543b49a
Matheus-Oliveira-Marino <mathson98@gmail.com>
2024-02-18 18:07:43 -0300

    atualizando template.bat

--- Servico/template.bat
+++ Servico/template.bat
@@ -1,6 +1,6 @@
 @echo off
-set NODE_MYSQL_SERVIDOR=localhost
-set NODE_MYSQL_USUARIO=maths
-set NODE_MYSQL_SENHA=matheus1998
-set NODE_MYSQL_BD=JOGOS
+set NODE_MYSQL_SERVIDOR=localhost/servidor_em_cloud
+set NODE_MYSQL_USUARIO=seu_usuario_do_banco_de_dados
+set NODE_MYSQL_SENHA=sua_senha_de_acesso_ao_banco_de_dados
+set NODE_MYSQL_BD=nome_do_seu_banco_de_dados
 @echo Variaveis de ambiente ajustadas com sucesso!
